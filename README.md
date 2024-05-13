013e2513d160fba3e5e93380ecc2f83f5f736288
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-05-13 18:11:27 +0900

    データベースを SQLite に変更。

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ WPF と Entity Framework Core 勉強用プロジェクト。
 * [CommunityToolkit.Mvvm](https://github.com/CommunityToolkit/dotnet)
 * [Microsoft.EntityFrameworkCore](https://learn.microsoft.com/ja-jp/ef/core/)
 * [Microsoft.EntityFrameworkCore.Relational](https://learn.microsoft.com/ja-jp/ef/core/what-is-new/nuget-packages)
-* [Microsoft.EntityFrameworkCore.SqlServer](https://learn.microsoft.com/ja-jp/ef/core/providers/?tabs=dotnet-core-cli)
+* [Microsoft.EntityFrameworkCore.SQLite](https://learn.microsoft.com/ja-jp/ef/core/providers/?tabs=dotnet-core-cli)
--- WpfEFCoreStudy/App.xaml.cs
+++ WpfEFCoreStudy/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using WpfEFCoreStudy.DataTypes;
+using WpfEFCoreStudy.DB;
 
 namespace WpfEFCoreStudy;
 
@@ -7,4 +10,36 @@ namespace WpfEFCoreStudy;
 /// </summary>
 public partial class App : Application
 {
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        this.CreateDatabase();
+    }
+
+    /// <summary>
+    /// データベースを作成する。
+    /// </summary>
+    private void CreateDatabase()
+    {
+        // データベースファイルを作成する。
+        using (BookDBContext dbContext = new BookDBContext())
+        {
+            dbContext.Database.EnsureCreated();
+
+            // 新規作成だった場合、サンプルデータを登録する。
+            if ((dbContext.Books.Count() == 0) && (dbContext.Authors.Count() == 0))
+            {
+                dbContext.Authors.Add(new Author() { AuthorName = "芥川龍之介" });
+                dbContext.Authors.Add(new Author() { AuthorName = "川端康成" });
+                dbContext.SaveChanges();
+
+                dbContext.Books.Add(new Book() { Title = "蜘蛛の糸", AuthorId = dbContext.Authors.Where(author => author.AuthorName == "芥川龍之介").First().AuthorId });
+                dbContext.Books.Add(new Book() { Title = "雪国", AuthorId = dbContext.Authors.Where(author => author.AuthorName == "川端康成").First().AuthorId });
+                dbContext.SaveChanges();
+            }
+        }
+    }
+
 }
