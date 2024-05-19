981c0ee9c4daca02a0c9e917ac6e6dcd7e55d515
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-05-19 19:16:54 +0900

    引数無し GetBooksAsync メソッドを削除。

--- WpfEFCoreStudy/Models/BookModel.cs
+++ WpfEFCoreStudy/Models/BookModel.cs
@@ -17,48 +17,11 @@ public sealed class BookModel
     /// <summary>
     /// 本情報を取得する。
     /// </summary>
-    /// <returns></returns>
-    public static async Task<IEnumerable<Book>> GetBooksAsync()
-    {
-        List<Book>? books = new List<Book>();
-
-        using (var dbContext = new BookDBContext())
-        {
-            // Left Join で取得。 <https://learn.microsoft.com/ja-jp/dotnet/csharp/linq/standard-query-operators/join-operations#perform-left-outer-joins>
-            books = await dbContext.Books
-                .GroupJoin(
-                    dbContext.Authors,
-                    book => book.AuthorId,
-                    author => author.AuthorId,
-                    (book, authors) => new { book, authors }
-                )
-                .SelectMany(
-                    bookAndAuthor => bookAndAuthor.authors.DefaultIfEmpty(),
-                    (bookAndAuthor, author) =>
-                    new Book()
-                    {
-                        BookId = bookAndAuthor.book.BookId,
-                        Title = bookAndAuthor.book.Title,
-                        AuthorId = bookAndAuthor.book.AuthorId,
-                        Author = author
-                    }
-                )
-                .ToListAsync();
-        }
-
-        return books;
-    }
-
-    /// <summary>
-    /// 本情報を取得する。
-    /// </summary>
     /// <param name="title">本のタイトル。部分一致検索する。</param>
     /// <param name="authorName">著者名。部分一致検索する。</param>
     /// <returns>本情報の一覧。</returns>
-    public static async Task<IEnumerable<Book>> GetBooksAsync(string title, string authorName)
+    public static async Task<IEnumerable<Book>> GetBooksAsync(string title = "", string authorName = "")
     {
-        List<Book> books = new List<Book>();
-
         using (BookDBContext dbContext = new BookDBContext())
         {
             ExpressionStarter<Book> predicateBuilder = PredicateBuilder.New<Book>(true);
@@ -72,7 +35,7 @@ public sealed class BookModel
             }
 
             // Left Join で取得。 <https://learn.microsoft.com/ja-jp/dotnet/csharp/linq/standard-query-operators/join-operations#perform-left-outer-joins>
-            books = await dbContext.Books
+            return await dbContext.Books
                 .GroupJoin(
                     dbContext.Authors,
                     book => book.AuthorId,
@@ -93,8 +56,6 @@ public sealed class BookModel
                 .Where(predicateBuilder)
                 .ToListAsync();
         }
-
-        return books;
     }
 
 }
